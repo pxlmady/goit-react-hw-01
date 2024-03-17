1e7eeed7465c2b2d36bfc999b629f4b91bc6e7cf
pxlmady <144046046+pxlmady@users.noreply.github.com>
2024-03-17 22:39:31 +0200

    Profile done

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Profile from "../Profile/Profile.jsx";
+import userData from "../../userData.json";
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      {/* <FriendList friends={friends} />
+      <TransactionHistory items={transactions} /> */}
+    </>
+  );
+};
+
+export default App;
